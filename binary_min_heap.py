4aeebdc4505531baf975aa1e7e1ee7fe71071f83
Karina Samvelyan <karina.samvelyan@gmail.com>
2020-12-20 15:53:40 +0200

    adding binary min heap and binary search tree

---
+++ binary_min_heap.py
@@ -0,0 +1,83 @@
+# binary min-heap
+# we're representing the heap using a list
+
+class binary_min_heap (object):
+	def __init__(self):
+		self.heap_list = []
+		self.length = 0
+
+	def __swap(self, i, j):
+		# swaps two elements in the heap
+		temp = self.heap_list[i]
+		self.heap_list[i] = self.heap_list[j]
+		self.heap_list[j] = temp
+
+	def insert(self, val):
+		self.heap_list.append(val)
+		self.length += 1
+		
+		j = self.length - 1 # current index of the inserted value
+		finished = False
+		while (j > 0) and (finished == False):
+			if (j % 2 == 0):
+				par_i = (j-2) // 2	# the parent index
+			else:	# j is odd
+				par_i = (j-1) // 2	# the parent index
+			if self.heap_list[par_i] > self.heap_list[j]:
+				# swap them!
+				self.__swap(j, par_i)
+				j = par_i
+			else:
+				finished = True
+
+	def __slide_down(self, i):
+		# sliding down an element at place i, if needed, to maintain (min) heap-property
+		leng = self.length
+		s = i 	# index of current element to be slided
+		while (s < leng):
+			if (2*s + 1 <= leng):	# if there exists left child
+				left_child = self.heap_list[2*s + 1]	
+				if (2*s + 1 <= leng):	# if there's also right child: (if no left, than also no right)
+					right_child = self.heap_list[2*s + 2]
+					# check who's smaller, if even one of then is smaller than the parent
+					if left_child < right_child:
+						swap_i = 2*s + 1
+					else:
+						swap_i = 2*s + 2
+				else:	# there's only left child
+					swap_i = 2*s + 1
+			# in case we need to swap:
+			if (self.heap_list[s] > self.heap_list[swap_i]):
+				self.__swap(s, swap_i)
+				s = swap_i
+			else: #no need to swap: stop!
+				break
+
+	def erase_by_index(self, i):
+		# erase element situated in place i
+		# method: swap it with the last one, delete, and slide the previously last one to a correct position.
+		last_i = self.length - 1
+		last = self.heap_list[last_i]	# last element
+		self.__swap(i, last_i)
+		
+		# erase new-last element and decrease length:
+		del self.heap_list[last_i]
+		self.length -= 1
+		# slide down the new i-th element, if needed:
+		self.__slide_down(i)
+
+	def min_val(self):
+		return (self.heap_list)[0]
+
+	def display_list(self):
+		return self.heap_list
+
+
+if __name__ == '__main__':
+	heap = binary_min_heap()
+	values = [5, 10, 7, 13, 20, 8, 16, 17, 14]
+	for val in values:
+		heap.insert(val)
+	print(heap.display_list())
+	heap.erase_by_index(0)
+	print(heap.display_list())
---
+++ binary_search_tree.py
@@ -0,0 +1,110 @@
+# implementing a binary search tree:
+
+class binary_search_tree (object) :
+	def __init__(self, val = None):
+		self.val = val
+		self.left_ch = None
+		self.right_ch = None
+
+	def isEmpty(self):
+		return self.val == self.left_ch == self.right_ch == None
+
+
+	def insert(self, val):
+		if (self.val is None) :
+			self.val = val
+		else:
+			cur_val = self.val
+			if (cur_val > val) :
+				if self.left_ch is None:
+					self.left_ch = binary_search_tree(val)
+				else:
+					self.left_ch.insert(val)
+			else:
+				if self.right_ch is None:
+					self.right_ch = binary_search_tree(val)
+				else:
+					self.right_ch.insert(val)
+
+	def display_ascend(self):
+		# displaying left-center-right bottom-up
+		if (self.val is None):
+			print('[empty]')
+		else:
+			if self.left_ch :
+				self.left_ch.display_ascend()
+			print(self.val)
+			if self.right_ch :
+				self.right_ch.display_ascend()
+
+	def sorted_list(self):
+		# going left-center(root)-right, bottom-up
+		if self.val is None:
+			return []
+		else:
+			sorted_l = []
+			if self.left_ch :
+				sorted_l = self.left_ch.sorted_list()
+			sorted_l.append(self.val)
+			if self.right_ch :
+				sorted_l = sorted_l + self.right_ch.sorted_list()
+			return sorted_l
+
+	def smallest(self):
+		if self.left_ch is None:
+			return self.val
+		else:
+			return self.left_ch.smallest()
+
+
+	def largest(self):
+		if self.right_ch is None:
+			return self.val
+		else:
+			return self.right_ch.largest()
+
+	def contains_value(self, q_val):
+		# returns True if the tree contains value, otherwise returns False.
+		if self.val :
+			if (self.val == q_val):
+				return True
+			elif (q_val < self.val) :
+				if self.left_ch : 
+					return (self.left_ch).contains_value(q_val)
+			else:
+				if self.right_ch :
+					return self.right_ch.contains_value(q_val)
+		return False
+	def print_tree(self):
+		# go root-[left-right]
+		if self.val :
+			print(self.val)
+			if self.left_ch :
+				print("/")
+				self.left_ch.print_tree()
+			if self.right_ch :
+				print("\\")
+				self.right_ch.print_tree()
+
+	def depth(self):
+		if self.val:
+			dep = 1
+			l_depth = 0
+			r_depth = 0
+			if self.left_ch:
+				l_depth = self.left_ch.depth()
+			if self.right_ch:
+				r_depth = self.right_ch.depth()
+			return dep + max(l_depth, r_depth)
+		return 0
+
+
+if __name__ == '__main__':
+	my_tree = binary_search_tree(5)
+	numbers = [2,7,3,0,5, 5, 1, -1, 9, 4]
+	for num in numbers:
+		my_tree.insert(num)
+
+	print("displaying in ascending order:")
+	print(my_tree.sorted_list())
+	print("tree depth is", my_tree.depth())
